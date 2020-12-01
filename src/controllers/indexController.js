e5934a419e2fe7c398d01674362e2a74bffb6d2f
Joaquin <joaquiperles@live.com.ar>
2020-12-01 15:53:39 -0300

    Objeto preference

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,56 @@
+const mercadopago = require('mercadopago')
+
+mercadopago.configure({
+  access_token:
+    'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
+  integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+})
+
 module.exports = {
-    home: (req, res) => {
-        return res.render("index");
-    },
-    detail: (req, res) => {
-        return res.render("detail", { ...req.query });
+  home: (req, res) => {
+    return res.render('index')
+  },
+  detail: (req, res) => {
+    return res.render('detail', { ...req.query })
+  },
+  comprar: (req, res) => {
+    let preference = {
+      payer: {
+        name: 'Ryan',
+        surname: 'Dahl',
+        email: 'test_user_63274575@testuser.com',
+        phone: {
+          area_code: '11',
+          number: 55556666,
+        },
+        address: {
+          zip_code: '1234',
+          street_name: 'Monroe',
+        },
+      },
+      items: [
+        {
+          id: '',
+          picture_url: '',
+          title: 'Zapatillas',
+          price: '',
+          description: '',
+          unit_price: 5,
+          quantity: 2,
+        },
+      ],
     }
+
+    mercadopago.preferences
+      .create(preference)
+      .then((response) => {
+        global.init_point = response.body.init_point
+        console.log(response)
+        res.render('confirm')
+      })
+      .catch((error) => {
+        console.log(error)
+        res.send('error')
+      })
+  },
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const indexController = require("../controllers/indexController");
+const indexController = require('../controllers/indexController')
 
 /* GET home page. */
-router.get('/', indexController.home);
+router.get('/', indexController.home)
 
 /* GET detail page */
-router.get('/detail', indexController.detail);
+router.get('/detail', indexController.detail)
 
-module.exports = router;
+/* GET buy page */
+router.post('/comprar', indexController.comprar)
+
+module.exports = router
